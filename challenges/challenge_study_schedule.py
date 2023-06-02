d3f1fb18d1dddf0cf8c7d4bd680f951cd1ee0ecf
Ikarosv <ikarovieiraa@gmail.com>
2023-06-02 16:35:22 -0300

    feat: study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(permanence_period: list[tuple[int, int]], target_time: int):
+    if not str(target_time).isnumeric():
+        return None
+    count = 0
+    for student in permanence_period:
+        if not str(student[0]).isnumeric() or not str(student[1]).isnumeric():
+            return None
+        if int(student[0]) <= target_time <= student[1]:
+            count += 1
+    return count
