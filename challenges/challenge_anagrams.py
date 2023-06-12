9fddfb878de184caeb1be23bbacec3a5f37cfaf7
Ikarosv <ikarovieiraa@gmail.com>
2023-06-12 16:58:45 -0300

    fix: problema de complexidade

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,68 +1,50 @@
 def is_anagram(first_string, second_string):
-    if not len(first_string) or not len(second_string):
-        return (sort_string(first_string), sort_string(second_string), False)
-    # Converter as strings para minúsculas
-    first_string = first_string.lower()
-    second_string = second_string.lower()
-
-    # Remover espaços em branco das strings
-    first_string = first_string.replace(" ", "")
-    second_string = second_string.replace(" ", "")
-
-    # Verificar se as strings têm o mesmo tamanho
-    if len(first_string) != len(second_string):
+    if not first_string or not second_string:
         return (sort_string(first_string), sort_string(second_string), False)
 
-    # Ordenar as strings
+    # Ordena as duas strings usando a função sort_string
     sorted_str1 = sort_string(first_string)
     sorted_str2 = sort_string(second_string)
-
-    # Verificar se as strings ordenadas são iguais
-    if sorted_str1 == sorted_str2:
-        return (sorted_str1, sorted_str2, True)
-    else:
-        return (sorted_str1, sorted_str2, False)
+    # Verifica se as strings ordenadas são iguais
+    return (sorted_str1, sorted_str2, sorted_str1 == sorted_str2)
 
 
-def sort_string(s):
-    # Converter a string em uma lista de caracteres
-    chars = list(s)
-
-    # Chamar a função de ordenação (exemplo usando o Merge Sort)
+def sort_string(string):
+    # Converte a string para minúsculas
+    string = string.lower()
+    # Converte a string em uma lista de caracteres
+    chars = list(string)
+    # Ordena a lista de caracteres usando o algoritmo de merge sort
     merge_sort(chars)
-
-    # Converter a lista de caracteres ordenada de volta para string
-    sorted_string = "".join(chars)
-
-    return sorted_string
+    # Retorna a string ordenada como uma única string
+    return "".join(chars)
 
 
 def merge_sort(arr):
     if len(arr) > 1:
         mid = len(arr) // 2
-        left_half = arr[:mid]
-        right_half = arr[mid:]
+        left = arr[:mid]
+        right = arr[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        merge(arr, left, right)
 
-        merge_sort(left_half)
-        merge_sort(right_half)
 
-        i = j = k = 0
-
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                arr[k] = left_half[i]
-                i += 1
-            else:
-                arr[k] = right_half[j]
-                j += 1
-            k += 1
-
-        while i < len(left_half):
-            arr[k] = left_half[i]
+def merge(arr, left, right):
+    i = j = k = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            arr[k] = left[i]
             i += 1
-            k += 1
-
-        while j < len(right_half):
-            arr[k] = right_half[j]
+        else:
+            arr[k] = right[j]
             j += 1
-            k += 1
+        k += 1
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
